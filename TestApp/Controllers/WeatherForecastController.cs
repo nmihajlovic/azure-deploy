9731a73524a62c158c60d0b58d6b8f97b7770c02
Nenad Mihajlovic <nenadmihajovic@rarecrew.com>
2020-01-28 12:17:27 +0100

    ~

--- TestApp/Controllers/WeatherForecastController.cs
+++ TestApp/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace TestApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hello","Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
